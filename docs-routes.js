29359a41fe1b4ae34d29c7f2a0bf51100170329c
Waldemart 🤘 <catindev@gmail.com>
2026-03-08 07:50:41 +0200

    Причесал шаблоны и код

--- docs-routes.js
+++ docs-routes.js
@@ -16,117 +16,97 @@
  *   GET /static/*             — CSS, JS, иконки
  */
 
-"use strict";
+'use strict';
 
-const path = require("path");
-const fs = require("fs");
-const ejs = require("ejs");
-const express = require("express");
+const path    = require('path');
+const fs      = require('fs');
+const ejs     = require('ejs');
+const express = require('express');
 
-const VIEWS_DIR = path.join(__dirname, "views");
-const STATIC_DIR = path.join(__dirname, "static");
+const VIEWS_DIR  = path.join(__dirname, 'views');
+const STATIC_DIR = path.join(__dirname, 'static');
 
 // Иконки — загружаем один раз при старте
 const ICON_NAMES = [
-  "big-logotype",
-  "big-pipline",
-  "big-pipline-library",
-  "big-rule",
-  "big-rule-library",
-  "big-condition",
-  "big-condition-library",
-  "big-dictionary",
-  "pipeline-list-icon",
-  "rule-list-icon",
-  "condition-list-icon",
-  "check-icon",
-  "check-icon-library",
-  "predicate-icon",
-  "predicate-icon-library",
-  "level-icon",
-  "level-icon-library",
-  "field-icon",
-  "field-icon-library",
-  "operator-icon",
-  "operator-icon-library",
-  "value-icon",
-  "value-icon-library",
-  "dictionary-icon",
-  "dictionary-icon-library",
+  'big-logotype',
+  'big-pipline', 'big-pipline-library',
+  'big-rule',    'big-rule-library',
+  'big-condition', 'big-condition-library',
+  'big-dictionary',
+  'pipeline-list-icon', 'rule-list-icon', 'condition-list-icon',
+  'pipeline-list-icon-library', 'rule-list-icon-library', 'condition-list-icon-library',
+  'check-icon', 'check-icon-library',
+  'predicate-icon', 'predicate-icon-library',
+  'level-icon', 'level-icon-library',
+  'field-icon', 'field-icon-library',
+  'operator-icon', 'operator-icon-library',
+  'value-icon', 'value-icon-library',
+  'dictionary-icon', 'dictionary-icon-library',
 ];
 const icons = {};
 for (const name of ICON_NAMES) {
-  const file = path.join(STATIC_DIR, "icons", name + ".svg");
-  icons[name] = fs.existsSync(file) ? fs.readFileSync(file, "utf8") : "";
+  const file = path.join(STATIC_DIR, 'icons', name + '.svg');
+  icons[name] = fs.existsSync(file) ? fs.readFileSync(file, 'utf8') : '';
 }
 
 function render(res, view, locals) {
-  const file = path.join(VIEWS_DIR, view + ".ejs");
-  ejs.renderFile(
-    file,
-    { ...locals, icons },
-    { views: VIEWS_DIR },
-    (err, html) => {
-      if (err) {
-        console.error("[docs] render error:", err.message);
-        return res.status(500).send("<pre>" + err.message + "</pre>");
-      }
-      res.setHeader("Content-Type", "text/html; charset=utf-8");
-      res.send(html);
-    },
-  );
+  const file = path.join(VIEWS_DIR, view + '.ejs');
+  ejs.renderFile(file, { ...locals, icons }, { views: VIEWS_DIR }, (err, html) => {
+    if (err) {
+      console.error('[docs] render error:', err.message);
+      return res.status(500).send('<pre>' + err.message + '</pre>');
+    }
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.send(html);
+  });
 }
 
 module.exports = function mountDocs(app, ctx) {
-  app.use("/static", express.static(STATIC_DIR));
+  app.use('/static', express.static(STATIC_DIR));
 
   // Главная — только корневые пайплайны (id без точки)
-  app.get("/", (req, res) => {
+  app.get('/', (req, res) => {
     const pipelines = [];
     for (const [id, a] of ctx.compiled.registry) {
-      if (a.type === "pipeline" && !id.includes(".")) pipelines.push(a);
+      if (a.type === 'pipeline' && !id.includes('.')) pipelines.push(a);
     }
     pipelines.sort((a, b) => a.id.localeCompare(b.id));
-    render(res, "home", { pipelines });
+    render(res, 'home', { pipelines });
   });
 
   // Пайплайн
-  app.get("/pipelines/:id", (req, res) => {
+  app.get('/pipelines/:id', (req, res) => {
     const a = ctx.compiled.registry.get(req.params.id);
-    if (!a || a.type !== "pipeline")
-      return res.status(404).send("Pipeline not found: " + req.params.id);
-    const cmp = ctx.compiled.pipelines && ctx.compiled.pipelines.get(a.id);
+    if (!a || a.type !== 'pipeline')
+      return res.status(404).send('Pipeline not found: ' + req.params.id);
+    const cmp   = ctx.compiled.pipelines && ctx.compiled.pipelines.get(a.id);
     const steps = cmp ? cmp.steps : [];
-    render(res, "pipeline", { pipeline: a, steps, compiled: ctx.compiled });
+    render(res, 'pipeline', { pipeline: a, steps, compiled: ctx.compiled });
   });
 
   // Правило
-  app.get("/rules/:id", (req, res) => {
+  app.get('/rules/:id', (req, res) => {
     const a = ctx.compiled.registry.get(req.params.id);
-    if (!a || a.type !== "rule")
-      return res.status(404).send("Rule not found: " + req.params.id);
-    render(res, "rule", { rule: a });
+    if (!a || a.type !== 'rule')
+      return res.status(404).send('Rule not found: ' + req.params.id);
+    render(res, 'rule', { rule: a });
   });
 
   // Условие
-  app.get("/conditions/:id", (req, res) => {
+  app.get('/conditions/:id', (req, res) => {
     const a = ctx.compiled.registry.get(req.params.id);
-    if (!a || a.type !== "condition")
-      return res.status(404).send("Condition not found: " + req.params.id);
-    render(res, "condition", { condition: a, compiled: ctx.compiled });
+    if (!a || a.type !== 'condition')
+      return res.status(404).send('Condition not found: ' + req.params.id);
+    render(res, 'condition', { condition: a, compiled: ctx.compiled });
   });
 
   // Справочник
-  app.get("/dictionaries/:id", (req, res) => {
+  app.get('/dictionaries/:id', (req, res) => {
     const a = ctx.compiled.registry.get(req.params.id);
-    if (!a || a.type !== "dictionary")
-      return res.status(404).send("Dictionary not found: " + req.params.id);
-    render(res, "dictionary", { dictionary: a });
+    if (!a || a.type !== 'dictionary')
+      return res.status(404).send('Dictionary not found: ' + req.params.id);
+    render(res, 'dictionary', { dictionary: a });
   });
 
-  console.log(
-    "[docs] UI available at http://localhost:" +
-      (process.env.PORT || 3000) +
-      "/",
-  );
+  console.log('[docs] UI available at http://localhost:' + (process.env.PORT || 3000) + '/');
 };
